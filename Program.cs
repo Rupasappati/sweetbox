733b98ee62fe13b3f6f7a32020f686f1a6f46fea
rupa <sappatir@ter.teradyne.com>
2024-01-07 17:33:05 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace calculator
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            calculator objCalc = new calculator();
+            Console.WriteLine("enter 1st number = ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter 2nd number = ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            objCalc.Addition(num1, num2);
+            objCalc.Sub(num1, num2);
+            objCalc.Mul(num1, num2);
+            objCalc.Div(num1, num2);
+
+        }
+    }
+
+}
---
+++ calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    public class calculator
+    {
+    public void Addition(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            Console.WriteLine("adition is : {0}", sum);
+        }
+
+        public void Sub(int num1, int num2)
+        {
+            int diff = 0;
+
+            if (num1 >= num2)
+            {
+                diff = num1 - num2;
+            }
+            else if (num1 <= num2)
+            {
+                diff = num2 - num1;
+            }
+            Console.WriteLine("Subtraction of {0} - {1} = {2}", num1, num2, diff);
+        }
+        public void Mul(int num1, int num2)
+        {
+            Console.WriteLine("Multiplication of {0} x {1} = {2}", num1, num2, num1 * num2);
+        }
+        public void Div(int num1, int num2)
+        {
+            Console.WriteLine("Division of {0} / {1} = {2}", num1, num2, num1 / num2);
+        }
+
+    }
+
+}
